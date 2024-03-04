2e3ac9c7cd6116f768a5f41887bd5f83b9f2ed04
NickScopre <71607068+NickScopre@users.noreply.github.com>
2024-03-03 19:07:47 -0500

    Update segment.py

--- segment.py
+++ segment.py
@@ -3,4 +3,17 @@
 
 def segment(img):
     
+    # Low Pass Filter to reduce noise
+
+    # Perform Grayscaling
+
+    # Apply Otsu's Algorithm of Thresholding
+
+    # Below Otsu's, Apply Region Growing based on seed point and pixel connectivity
+
+    # Above Otsu's, Apply Edge Detection and Edge Thinning
+
+    # Combine above two images with original image
+
+
     return img
