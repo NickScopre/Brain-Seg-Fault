cc83915cf5c041ca29bf9f79ef3d9bc1b7f64842
NickScopre <71607068+NickScopre@users.noreply.github.com>
2024-03-05 17:11:15 -0500

    Main Commit
    The actually functionality of the program. In testing, it's not perfect and its weakness really shows when the tumor doesn't have a more unique color range, but it does really excel at identifying some instances of tumors.

--- segment.py
+++ segment.py
@@ -1,19 +1,70 @@
-
-
+import cv2
+import numpy as np
 
 def segment(img):
     
     # Low Pass Filter to reduce noise
+    img1 = cv2.GaussianBlur(img, (11, 11), 0)
 
-    # Perform Grayscaling
+    ## Perform Grayscaling
+    # Apply Contrast Limited Adaptive Histogram Equalization (CLAHE)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    img2 = clahe.apply(img1)
+
+    # Gray Level Slicing
+    intensity_ranges = [
+        (0, 50, 0),
+        (100, 255, 0)
+    ]
+
+    # Apply gray level slicing for each intensity range mapping
+    grayscaling = np.copy(img2)
+    for intensity_range in intensity_ranges:
+        min_intensity, max_intensity, slice_intensity = intensity_range
+        grayscaling[(img2 >= min_intensity) & (img2 <= max_intensity)] = slice_intensity
+
+
+    #img4 = cv2.GaussianBlur(img3, (11, 11), 0)
+    # Apply Contrast Limited Adaptive Histogram Equalization (CLAHE)
+    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(3, 3))
+    clahe = clahe.apply(grayscaling)
+
+    min_range = int(np.ceil(max(img.shape)*0.03))
+    min = cv2.erode(clahe, np.ones((min_range, min_range), np.uint8))
+
+    # Gray Level Slicing
+    intensity_ranges = [
+        (0, 15, 0),
+        (80, 255, 0)
+    ]
+
+    # Apply gray level slicing for each intensity range mapping
+    img6 = np.copy(min)
+    for intensity_range in intensity_ranges:
+        min_intensity, max_intensity, slice_intensity = intensity_range
+        img6[(min >= min_intensity) & (min <= max_intensity)] = slice_intensity 
+
+    img7 = cv2.GaussianBlur(img6, (11, 11), 0)
+
+    # Clip to Ensure in Range
+    img8 = np.clip(img7, 0, 255).astype(np.uint8)
 
     # Apply Otsu's Algorithm of Thresholding
+    _, otsu = cv2.threshold(img8, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    #otsus_img = cv2.bitwise_not(otsus_img)
+    #above_img = cv2.bitwise_and(img, img, mask=otsus_img)
 
-    # Below Otsu's, Apply Region Growing based on seed point and pixel connectivity
+    # Canny's Edge Detection
+    canny = cv2.Canny(min, 100, 200)
 
-    # Above Otsu's, Apply Edge Detection and Edge Thinning
+    # somethin
+    border = cv2.dilate(canny, np.ones((3, 3), np.uint8))
 
-    # Combine above two images with original image
+    final = np.clip(img.astype(int)-50, 0, 255).astype(np.uint8) # Darken original image
+    map1 = (otsu == 255) # Create mask for Otsu's Output
+    map2 = (border == 255) # Create mask for Canny's Border
+    final[map1] = np.clip(final[map1].astype(int) + 50, 0, 255).astype(np.uint8)
+    final[map2] = np.clip(final[map2].astype(int) + 75, 0, 255).astype(np.uint8) 
 
-
-    return img
+    # Return order: Grayscaling, CHALE, Min Pooling, Otsu's Output, Canny's, s, Final
+    return grayscaling, clahe, min, otsu, canny, border, final
--- test.py
+++ test.py
@@ -1,3 +1,72 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import shutil as sh
+from segment import segment
+
+folder_path = "C:\\Users\\nicho\\OneDrive\\Desktop\\Visual Studio\\Python\\CS659\\Brain-Seg-Local\\input"
+subfolder_name = 'Astrocitoma T1'
+image_name = 'Astrocitoma T1_1.jpeg'
+image_path = os.path.join(folder_path, subfolder_name, image_name)
+
+fig, axes = plt.subplots(2, 4)
+
+img = plt.imread(image_path)
+if len(img.shape) > 2:
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
+# Original
+axes[0][0].imshow(img, cmap="gray")
+axes[0][0].set_title("Original Image")
+axes[0][0].set_xticks([])
+axes[0][0].set_yticks([])
+
+gray, clahe, min, otsu, canny, border, final = segment(img)
+
+# Grayscaling
+axes[0][1].imshow(gray, cmap="gray")
+axes[0][1].set_title("Gray Level Slicing")
+axes[0][1].set_xticks([])
+axes[0][1].set_yticks([])
+
+# CLAHE
+axes[0][2].imshow(clahe, cmap="gray")
+axes[0][2].set_title("CLAHE")
+axes[0][2].set_xticks([])
+axes[0][2].set_yticks([])
+
+# Min Pooling
+axes[0][3].imshow(min, cmap="gray")
+axes[0][3].set_title("Min Pooling")
+axes[0][3].set_xticks([])
+axes[0][3].set_yticks([])
+
+# Otsu's Thresholding
+axes[1][0].imshow(otsu, cmap="gray")
+axes[1][0].set_title("Otsu's")
+axes[1][0].set_xticks([])
+axes[1][0].set_yticks([])
+
+# Canny's Edge Detection
+axes[1][1].imshow(canny, cmap="gray")
+axes[1][1].set_title("Canny's")
+axes[1][1].set_xticks([])
+axes[1][1].set_yticks([])
+
+# Dialated Border
+axes[1][2].imshow(border, cmap="gray")
+axes[1][2].set_title("Border")
+axes[1][2].set_xticks([])
+axes[1][2].set_yticks([])
+
+# Final
+axes[1][3].imshow(final, cmap="gray")
+axes[1][3].set_title("Final Output")
+axes[1][3].set_xticks([])
+axes[1][3].set_yticks([])
+
+
+plt.show()
 
