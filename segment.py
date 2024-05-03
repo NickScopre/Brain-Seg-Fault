a460292c5f01311c10532d130372662b5f331b20
NickScopre <71607068+NickScopre@users.noreply.github.com>
2024-05-02 21:00:27 -0400

    Major Edits
    An expansion of the program that more effectively shows the vital regions and improves visual clarity through deliberate applications of low pass filters and adjustments of many parameters.

--- segment.py
+++ segment.py
@@ -6,32 +6,42 @@ def segment(img):
     # Low Pass Filter to reduce noise
     img1 = cv2.GaussianBlur(img, (11, 11), 0)
 
-    # Apply Contrast Limited Adaptive Histogram Equalization (CLAHE)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    img2 = clahe.apply(img1)
+    # Apply Contrast Limited Adaptive Histogram Equalization (CLAHE) (was 2.0)
+    clahe_inst = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(15, 15))
+    clahe = clahe_inst.apply(img1)
+
+    # Low Pass Filter
+    clahe_blur = cv2.GaussianBlur(clahe, (51, 51), 0)
 
     # Gray Level Slicing
     intensity_ranges = [
-        (0, 50, 0),
+        (0, 45, 0),
+        (46, 55, 150),
+        (56, 75, 200),
+        (76, 99, 150),
         (100, 255, 0)
     ]
-
+    
     # Apply gray level slicing for each intensity range mapping
-    grayscaling = np.copy(img2)
+    grayscaling = np.copy(clahe_blur)
     for intensity_range in intensity_ranges:
         min_intensity, max_intensity, slice_intensity = intensity_range
-        grayscaling[(img2 >= min_intensity) & (img2 <= max_intensity)] = slice_intensity
+        grayscaling[(clahe_blur >= min_intensity) & (clahe_blur <= max_intensity)] = slice_intensity
 
 
     # Apply Contrast Limited Adaptive Histogram Equalization (CLAHE)
-    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(3, 3))
-    clahe = clahe.apply(grayscaling)
+    clahe2 = cv2.createCLAHE(clipLimit=25.0, tileGridSize=(5, 5))
+    clahe2 = clahe2.apply(grayscaling)
+
+    # Second Low Pass Filter
+    clahe2_blur = cv2.GaussianBlur(clahe2, (25, 25), 0)
 
     # Minimum Kernel
-    min_range = int(np.ceil(max(img.shape)*0.03))
-    min = cv2.erode(clahe, np.ones((min_range, min_range), np.uint8))
+    min_range = int(np.ceil(max(img.shape)*0.04))
+    min = cv2.erode(clahe2_blur, np.ones((min_range, min_range), np.uint8))
 
-    img6 = cv2.GaussianBlur(min, (11, 11), 0)
+    # Third Low Pass Filter 
+    img6 = cv2.GaussianBlur(min, (201, 201), 0)
 
     # Clip to Ensure in Range
     img7 = np.clip(img6, 0, 255).astype(np.uint8)
@@ -40,15 +50,18 @@ def segment(img):
     _, otsu = cv2.threshold(img7, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     # Canny's Edge Detection
-    canny = cv2.Canny(min, 100, 200)
+    canny = cv2.Canny(otsu, 100, 200)
 
-    # somethin
+    # Border
     border = cv2.dilate(canny, np.ones((3, 3), np.uint8))
 
-    final = np.clip(img.astype(int)-50, 0, 255).astype(np.uint8) # Darken original image
+    # Darken original image
+    final = np.clip(img.astype(int)-50, 0, 255).astype(np.uint8) 
+
+    #Outline
     map1 = (otsu == 255) # Create mask for Otsu's Output
     map2 = (border == 255) # Create mask for Canny's Border
-    final[map1] = np.clip(final[map1].astype(int) + 50, 0, 255).astype(np.uint8)
+    final[map1] = np.clip(final[map1].astype(int) + 25, 0, 255).astype(np.uint8)
     final[map2] = np.clip(final[map2].astype(int) + 75, 0, 255).astype(np.uint8) 
 
-    return clahe, grayscaling, min, otsu, canny, border, final
+    return img1, clahe, clahe_blur, grayscaling, clahe2, clahe2_blur, min, img6, otsu, border, final
--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import os
-import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import shutil as sh
 from segment import segment
 
 folder_path = "C:\\Users\\nicho\\OneDrive\\Desktop\\Visual Studio\\Python\\CS659\\Brain-Seg-Local\\input"
@@ -10,7 +8,7 @@ subfolder_name = 'Astrocitoma T1'
 image_name = 'Astrocitoma T1_1.jpeg'
 image_path = os.path.join(folder_path, subfolder_name, image_name)
 
-fig, axes = plt.subplots(2, 4)
+fig, axes = plt.subplots(3, 4)
 
 img = plt.imread(image_path)
 if len(img.shape) > 2:
@@ -23,50 +21,73 @@ axes[0][0].set_title("Original Image")
 axes[0][0].set_xticks([])
 axes[0][0].set_yticks([])
 
-clahe, gray, min, otsu, canny, border, final = segment(img)
+blur, clahe, clahe_blur, grayscaling, clahe2, clahe2_blur, min, min_blur, otsu, canny, final = segment(img)
 
-# CLAHE
-axes[0][1].imshow(clahe, cmap="gray")
-axes[0][1].set_title("CLAHE")
+# BLUR
+axes[0][1].imshow(blur, cmap="gray")
+axes[0][1].set_title("Low Pass Filter")
 axes[0][1].set_xticks([])
 axes[0][1].set_yticks([])
 
-# Grayscaling
-axes[0][2].imshow(gray, cmap="gray")
-axes[0][2].set_title("Gray Level Slicing")
+# CLAHE
+axes[0][2].imshow(clahe, cmap="gray")
+axes[0][2].set_title("CLAHE")
 axes[0][2].set_xticks([])
 axes[0][2].set_yticks([])
 
-
-# Min Pooling
-axes[0][3].imshow(min, cmap="gray")
-axes[0][3].set_title("Min Kernel")
+# CLAHE BLUR
+axes[0][3].imshow(clahe_blur, cmap="gray")
+axes[0][3].set_title("Low Pass Filter")
 axes[0][3].set_xticks([])
 axes[0][3].set_yticks([])
 
-# Otsu's Thresholding
-axes[1][0].imshow(otsu, cmap="gray")
-axes[1][0].set_title("Otsu's")
+# GRAYSCALING
+axes[1][0].imshow(grayscaling, cmap="gray")
+axes[1][0].set_title("Grayscaling")
 axes[1][0].set_xticks([])
 axes[1][0].set_yticks([])
 
-# Canny's Edge Detection
-axes[1][1].imshow(canny, cmap="gray")
-axes[1][1].set_title("Canny's")
+# CLAHE 2
+axes[1][1].imshow(clahe2, cmap="gray")
+axes[1][1].set_title("CLAHE 2")
 axes[1][1].set_xticks([])
 axes[1][1].set_yticks([])
 
-# Dialated Border
-axes[1][2].imshow(border, cmap="gray")
-axes[1][2].set_title("Border")
+# CLAHE 2 BLUR
+axes[1][2].imshow(clahe2_blur, cmap="gray")
+axes[1][2].set_title("Low Pass Filter")
 axes[1][2].set_xticks([])
 axes[1][2].set_yticks([])
 
-# Final
-axes[1][3].imshow(final, cmap="gray")
-axes[1][3].set_title("Final Output")
+# MIN
+axes[1][3].imshow(min, cmap="gray")
+axes[1][3].set_title("Min Kernel")
 axes[1][3].set_xticks([])
 axes[1][3].set_yticks([])
+
+# MIN BLUR
+axes[2][0].imshow(min_blur, cmap="gray")
+axes[2][0].set_title("Low Pass Filter")
+axes[2][0].set_xticks([])
+axes[2][0].set_yticks([])
+
+# OTSU
+axes[2][1].imshow(otsu, cmap="gray")
+axes[2][1].set_title("Otsu's")
+axes[2][1].set_xticks([])
+axes[2][1].set_yticks([])
+
+# CANNY
+axes[2][2].imshow(canny, cmap="gray")
+axes[2][2].set_title("Canny's")
+axes[2][2].set_xticks([])
+axes[2][2].set_yticks([])
+
+# FINAL
+axes[2][3].imshow(final, cmap="gray")
+axes[2][3].set_title("Final")
+axes[2][3].set_xticks([])
+axes[2][3].set_yticks([])
 
 
 plt.show()
